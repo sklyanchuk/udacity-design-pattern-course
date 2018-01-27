658bf18c8627c15832b1f044d0e3fdda6dfc34af
Artem Sklyanchuk <artem.sklyanchuk@gmail.com>
2018-01-28 00:17:22 +0300

    toggle cat's info on cat's name click

--- script.js
+++ script.js
@@ -6,16 +6,22 @@ const Cat = function(name, src) {
 
 const cat1 = new Cat('Barsic', 'img/cat0.jpg');
 const cat2 = new Cat('Murzic', 'img/cat1.jpg');
+const cat3 = new Cat('Pashtet', 'img/cat2.jpg');
+const cat4 = new Cat('Pusha', 'img/cat3.jpg');
+const cat5 = new Cat('Rizhick', 'img/cat4.jpg');
+const cat6 = new Cat('Princess', 'img/cat5.jpeg');
 
-let cats = [cat1, cat2];
+let cats = [cat1, cat2, cat3, cat4, cat5, cat6];
 
 cats.forEach((cat, index) => {
-	const catHTML = document.createElement('li');
+	const catHTML = document.createElement('div');
 	catHTML.innerHTML = `
 		<h3 class="cat-name">${cat.name}</h3>
-		<img src="${cat.src}" class="clicks-count-img clicks-count-img-${index}">
-  	<input type="number" class="clicks-count" value="${cat.clicks}" readonly>
-  	<button class="clicks-count-reset">Reset!</button>
+		<div class="cat-info">
+			<img src="${cat.src}" class="clicks-count-img clicks-count-img-${index}">
+	  	<input type="number" class="clicks-count" value="${cat.clicks}" readonly>
+	  	<button class="clicks-count-reset">Reset!</button>
+		</div>
 	`;
 	document.querySelector('.all-cats').appendChild(catHTML);
 });
@@ -30,5 +36,11 @@ document.querySelectorAll('.clicks-count-img').forEach((elem) => {
 document.querySelectorAll('.clicks-count-reset').forEach((elem) => {
 	elem.addEventListener('click', (event) => {
   	event.target.parentElement.querySelector('.clicks-count').value = 0;
+	}, false);
+});
+
+document.querySelectorAll('.cat-name').forEach((elem) => {
+	elem.addEventListener('click', (event) => {
+		event.target.parentElement.classList.toggle('hidden-info');
 	}, false);
 });
