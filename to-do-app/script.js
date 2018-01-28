e88e452caf2a4e3b8c7896d96483d851afc0c36e
Artem Sklyanchuk <artem.sklyanchuk@gmail.com>
2018-01-29 00:27:32 +0300

    my first to-do app :)

---
+++ to-do-app/script.js
@@ -0,0 +1,59 @@
+const Model = {
+  init() {
+    if (!localStorage.todoItems) {
+      localStorage.todoItems = JSON.stringify([]);
+    }
+  },
+  add(todoObj) {
+    let data = JSON.parse(localStorage.todoItems);
+    data.push(todoObj);
+    localStorage.todoItems = JSON.stringify(data);
+  },
+  getAllTodoItems() {
+    console.log(JSON.parse(localStorage.todoItems));
+    return JSON.parse(localStorage.todoItems);
+  }
+}
+
+const Controller = {
+  init() {
+    Model.init();
+    View.init();
+  },
+  addToDoItem(todoItem) {
+    Model.add({
+      todoItem: todoItem,
+      dateCreated: new Date()
+    })
+    View.render();
+  },
+  getNotes() {
+    console.log(Model.getAllTodoItems());
+    return Model.getAllTodoItems();
+  }
+}
+
+const View = {
+  init() {
+    this.todoList = document.querySelector('.to-do-list');
+    document.querySelector('.add-to-do-item').addEventListener('click', (event) => {
+      let todoItem = document.querySelector('.to-do-name').value;
+      if (todoItem) {
+        console.log(Controller);
+        Controller.addToDoItem(todoItem);
+        document.querySelector('.to-do-name').value = '';
+      }
+      event.preventDefault();
+    });
+    this.render();
+  },
+  render() {
+    let htmlStr = '';
+    Controller.getNotes().forEach((todo) => {
+        htmlStr += '<div><h3 class="note">'+ todo.todoItem + '</h3><p>' + new Date(todo.dateCreated).toLocaleDateString() + '</p></div>';
+    });
+    this.todoList.innerHTML = htmlStr;
+  }
+}
+
+Controller.init();
